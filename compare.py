04c289f947c59b68b3bb5ac94227fff4019cbfbb
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-08-10 19:33:04 +0200

    Remove blank lines and comments

--- compare.py
+++ compare.py
@@ -4,7 +4,8 @@ import re
 class ReqDicts(object):
     def __init__(self, req_file):
         self._dicts = [self.line_to_dict(line)
-                       for line in open(req_file)]
+                       for line in open(req_file)
+                       if line.strip() and not line.startswith('#')]
 
     def dicts(self):
         return self._dicts
@@ -12,23 +13,15 @@ class ReqDicts(object):
     def line_to_dict(self, line):
         regexp = ''        # version, defined to be anything between [ and ]
 
-        if line.strip() and not line.startswith('#'):
-            m = re.findall('[a-zA-Z-]+', line)
-            v = re.findall('[0-9.]+', line)
+        m = re.findall('[a-zA-Z-]+', line)
+        v = re.findall('[0-9.]+', line)
 
+        if m:
+            pkg = m
+            vrs = v
+            output = {'pkg': pkg, 'vrs': vrs}
 
-            if m:
-                pkg = m
-                vrs = v
-                output = {'pkg': pkg,
-                          'vrs': vrs}
-
-            else:
-                output = None
-        else:
-            output = None
-   
-        return output
+            return output
 
 
 if __name__ == '__main__':
