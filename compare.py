9ea63bd380ac2448cbca81e5784912cfdfa840fc
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-08-30 10:32:54 +0200

    create dictionary for each environment

--- compare.py
+++ compare.py
@@ -1,67 +1,71 @@
+import difflib
 import os
 import re
+import sys
 from collections import defaultdict
 
 
 class ReqDicts(object):
     def __init__(self, file1, file2):
-        self.servers = defaultdict(lambda: defaultdict(list))
         self.current_server = ''
-        self.filename = os.path.basename(file1)
-        self.file1_feeder = [self.line2dict(line)
-                             for line in open(file1, 'r')
-                             if line.strip() and not line.startswith('#')]
-        self.file2_feeder = [self.line2dict(line)
-                             for line in open(file2, 'r')
-                             if line.strip() and not line.startswith('#')]
+        self.filename1 = os.path.basename(file1)
+        self.filename2 = os.path.basename(file2)
+        self.env1 = self.file_2_dict(file1)
+        self.env2 = self.file_2_dict(file2)
 
-    def line2dict(self, line):
-        server_name = re.search(r'''([A-Z]+[0-9]+)''', line, re.X)
-        module_version = re.search(r'''([a-zA-Z-]+).*(\d\.\d\.\d)''', line, re.X)
+    def __repr__(self):
+        # doesn't work
+        output = ''
+        for server, modules in self.env1.items():
+            output += server
+            for module, version in modules.items():
+                output += module
+        return output
 
-        if 'modules deployed to' in line:
-            self.current_server = server_name.group(1)
+    def file_2_dict(self, file):
+        file_dict = {}
 
-        elif module_version:
-            # save the module and version
-            self.servers[self.current_server][module_version.group(1)].append(module_version.group(2))
+        for line in open(file, 'r'):
+
+            server_name = re.search(r'''([A-Z]+[0-9]+)''', line, re.X)
+            module_version = re.search(r'''([a-zA-Z-]+).*(\d\.\d\.\d)''', line, re.X)
+
+            if server_name:
+                self.current_server = server_name.group(1)
+                file_dict[self.current_server] = {}
+
+            if module_version:
+                file_dict[self.current_server][module_version.group(1)] = (module_version.group(2))
+
+        return file_dict
 
     def dicts(self, key=None):
         return list(self.iterdicts(key=key))
 
-    def iterdicts(self, key=None):
+    def iter_dicts(self, key=None):
         if key:
             return (item
-                    for item in sorted(self.servers, key=key))
+                    for item in sorted(self.env1, key=key))
         else:
             return (item
-                    for item in self.servers)
+                    for item in self.env1)
+
+    def flatten_dict(self):
+        self.env1 = [modules
+                     for server in self.env1.items()
+                     for modules, versions in server
+                     if versions[0]]
+
+        self.env2 = [modules
+                     for server in self.env2.items()
+                     for modules in server]
 
 
 if __name__ == '__main__':
-    check_deploy_int = 'files_2_compare/int.txt'
-    check_deploy_prod = 'files_2_compare/prod.txt'
+    deploy_int = 'files_2_compare/int.txt'
+    deploy_prod = 'files_2_compare/prod.txt'
 
-    ckInt = ReqDicts(check_deploy_int, check_deploy_prod)
-    # ckProd = ReqDicts()
+    check_deployment = ReqDicts(deploy_int, deploy_prod)
 
-    print(ckInt.servers)
-
-    """
-   
-    if ckInt.dicts() == ckProd.dicts():
-        print('The deployment matches!')
-    else:
-        print('The deployments are different!')
-
-    print(f'{ckInt.filename:35} {ckProd.filename}')
-
-    """
-
-    for server, modules in ckInt.servers.items():
-        print(server)
-        for module, version in modules.items():
-            print(f'{module:15}{version}')
-
-        print()
-
+    print(f'Environment 1 is {check_deployment.env1}')
+    print(f'Environment 2 is {check_deployment.env2}')
